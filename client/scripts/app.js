40b6bb3f4fc9e779cd9801f356467be25b9e0bf1
mishasaggi <monisha.saggi@gmail.com>
2016-01-18 12:19:35 -0600

    Create app object, add underscore to bower json and index.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,32 @@
-// YOUR CODE HERE:
+var app = {
+  url: 'https://api.parse.com/1/classes/chatterbox',
+
+  init: function(){
+  },
+
+  send: function(message){
+  },
+
+  fetch: function(){
+  },
+
+  clearMessages: function(){
+  },
+
+  addMessage: function(messageObject){
+  },
+
+  addRoom: function(){
+  },
+
+  addFriend: "", //property or function
+  
+  handleSubmit: function(){
+  }
+};
+//run the init function to kick start the app!
+app.init();
+
+
+
 
