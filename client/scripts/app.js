7ee1d7069f44aadd50a98f11ae41397aba15ce04
mishasaggi <monisha.saggi@gmail.com>
2016-01-18 12:21:47 -0600

    Add psuedocode and test message.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,26 +1,39 @@
+// var testMessage = {
+//           username: 'Somthing',
+//           text: 'my message!'
+//         }; //passed hardcoded testing!!!
+
 var app = {
   url: 'https://api.parse.com/1/classes/chatterbox',
 
   init: function(){
+    //the initialization function should call fetch method
+    app.fetch();
   },
 
   send: function(message){
+    //ajax request to post message to the server
   },
 
   fetch: function(){
+    //ajax request to get all messages from the server
   },
 
   clearMessages: function(){
+    //clears messages from the DOM
   },
 
   addMessage: function(messageObject){
+    //clear messages to avoid duplicacy
+  
+    //loop through the messages and append to DOM
   },
 
   addRoom: function(){
   },
 
   addFriend: "", //property or function
-  
+
   handleSubmit: function(){
   }
 };
