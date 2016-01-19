22d3b716568e24642a90ae4dc416e90f1cab0328
mishasaggi <monisha.saggi@gmail.com>
2016-01-19 14:07:52 -0600

    Add event handler and unique rooms.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -9,7 +9,19 @@ var app = {
   init: function(){
     //the initialization function should call fetch method
     app.fetch(); //first call on loading the page
-    setInterval(app.fetch, 5000); //auto refresh every 5 seconds
+    setInterval(app.fetch, 10000); //auto refresh every 5 seconds
+    $(document).ready(function(){
+      $('#chat-form').on('submit', function(event){
+        //prevent default
+        event.preventDefault();
+
+        //grab user input values
+        var $inputname = $('#user-name').val();
+        var $inputtext = $('#user-text').val();
+        var $inputroom = $('#user-room').val();
+        app.handleSubmit($inputname, $inputtext, $inputroom);
+      });
+    });
 
   },
   send: function(message){
@@ -32,6 +44,7 @@ var app = {
     $.ajax({
       url: app.url,
       type: 'GET',
+      data: {order: '-createdAt'},
       contentType: 'application/JSON', //receives 401 without specifying this field
       success: function(data){
         console.log("data recieved from server", data);
@@ -62,28 +75,79 @@ var app = {
     var $username = message.username;
     var $usertext = message.text;
     var $timestamp = message.createdAt;
+    var $userroom = message.room;
 
+    //add default room
+    if(!$userroom) $userroom = 'lobby';
     //create a div to hold a single message
     var postDiv = $("<div></div>");
     var usernameDiv = $("<div></div>");
     var textDiv = $("<div></div>");
 
-    usernameDiv.append($username);
+
+    usernameDiv.append($username).append(' in ').append($userroom);
     textDiv.append($usertext).append(' ').append($timestamp);
 
     postDiv.append(usernameDiv);
     postDiv.append(textDiv);
 
     $("#all-chats").append(postDiv).append('</br>');
+
+    //call addRoom if unique
+    //store room names in an object, keys are unique
+    //might be a redundant step if we have to check before appending to the dropdown too.
+    // if(!app.rooms[$userroom]) {
+    //   app.rooms[$userroom] = $userroom;
+    // }
+    // console.log(app.rooms);
+    // for(var key in app.rooms) {
+      //append only if it in not in the drop down 
+
+      //test addition
+      // var newOption = $('<option value="dummy">dummy</option>');
+      // $('#rooms').append(newOption);
+
+      //$('#rooms')[0]) gives the object(array) containing all select options
+      //$('#rooms')[0][0]) gives the first select option row
+
+      //set flag
+      var haveRoom = false;
+
+      $('#rooms option').each(function(){
+        console.log($(this).val(), "this");
+        if( $(this).val() === $userroom) {
+          console.log("found room");
+          haveRoom = true;
+        }
+      });
+      if(!haveRoom) {
+        var newOption = $('<option value="'+ $userroom +'">'+ $userroom +'</option>');
+        $('#rooms').append(newOption);
+      }
+      
+    // };
+
+    // app.addRoom($userroom);
   },
+  rooms: { },
+
   addRoom: function(){
+    //have a drop down with room names, keep unique values only.
 
   },
   addFriend: "", //property or function
   
-  handleSubmit: function(){
-    //prevent default
+  handleSubmit: function(username, usertext, userroom){
+    
+    console.log("arguments recieved by the handle submit", username, usertext);
+    //create message object to send to the server
+    var message = {
+          username: username,
+          text: usertext,
+          room: userroom
+        };
 
+    app.send(message);
   }
 
 };
